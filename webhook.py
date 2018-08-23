65958a53ac1018c37120272aa5e8a9ad5f22f0d7
amanosalva94 <amanosalva@bytesw.com>
2018-08-23 14:25:36 -0500

    asd

--- webhook.py
+++ webhook.py
@@ -36,7 +36,7 @@ def makeResponse(req):
     intentName = metadata.get("intentName")
     
     if intentName == "no.secuencial.no.parametrica.pg1-options":
-        parameters = req.get("parameters")
+        parameters = result.get("parameters")
         tiposdeproducto = parameters.get("tiposdeproducto")
 
         if tiposdeproducto == "Cuenta.Sueldo":
@@ -58,131 +58,6 @@ def makeResponse(req):
 				}
 			]
             }
-            
-    if intentName == "promocion.webhook":             
-        result = req.get("result")
-        parameters = result.get("parameters")
-        nombre = parameters.get("promo")
-        if nombre is None:
-            return None
-        r=requests.get('http://181.177.228.114:8095/items/')
-        json_object = r.json()
-        promociones=json_object['promociones']
-        for i in range(0,30):
-            if nombre in promociones[i]['nombre']:
-                descripcionPromo = promociones[i]['descripcion']
-                moneda = promociones[i]['moneda']
-                costo = promociones[i]['costo']
-                break
-        speech = "La promoción nombre consta de "+descripcionPromo+ " a tan solo "+moneda+" "+costo + "!!!"
-        print(speech)
-        return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "apiai-weather-webhook"
-        }
-
-
-    if intentName == "test-facebook-webhook":             
-        result = req.get("result")
-        parameters = result.get("parameters")
-        nombre = parameters.get("promo")        
-        return {
-        "speech": "speech",
-        "displayText": "speech",
-        "source": "apiai-weather-webhook",
-        "messages": [
-				{
-					"type": "simple_response",
-					"platform": "google",
-					"textToSpeech": "Te escribo desde el webhook! - textToSpeech"
-				},
-				{
-					"type": 3,
-                    "platform": "facebook",
-                    "imageUrl": "https://firebasestorage.googleapis.com/v0/b/dialogflowproyect.appspot.com/o/Para-qu%C3%A9-sirve-un-histograma_opt.png?alt=media&token=1ca38de2-5c07-4bff-bcea-7f3e60d78c11"
-				},
-				{
-					"type": 0,
-					"speech": "Lo siento, aún no configuro nada para whatsapbi"
-				}
-			]
-        }
-
-    if intentName == "test-facebook-webhook-card":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        cliente_id = parameters.get("cliente_id")              
-        r=requests.get('http://181.177.228.114:5000/grafico/'+str(cliente_id))
-        json_object = r.json()
-        url_image=json_object['url_image']       
-        return {
-        "speech": "speech",
-        "displayText": "speech",
-        "source": "apiai-weather-webhook",
-        "messages": [
-				{
-					"type": "simple_response",
-					"platform": "google",
-					"textToSpeech": "Te escribo desde el webhook! - textToSpeech"
-				},
-				{
-					"type": 3,
-                    "platform": "facebook",
-                    "imageUrl": url_image
-				},
-				{
-					"type": 0,
-					"speech": "Lo siento, aún no configuro nada para whatsapbi"
-				}
-			]
-        }    
-
-
-    if intentName == "promocion.planes.consultar":             
-        result = req.get("result")       
-        
-        r=requests.get('http://181.177.228.114:8095/items/')
-        json_object = r.json()
-        promociones=json_object['promociones']
-        descripcionPromo = []
-        for i in range(0,len(promociones)):
-            temp = []
-            temp.append(promociones[i]['descripcion'])
-            descripcionPromo.append(temp)
-            speech = "Te escribo desde el webhook! - speech"
-			
-        	
-        return {
-			"speech": speech,
-			"displayText": speech,
-			"source": "apiai-weather-webhook",
-			"messages": [
-				{
-					"type": "simple_response",
-					"platform": "google",
-					"textToSpeech": "Te escribo desde el webhook! - textToSpeech"
-				},
-				{
-					"type": 4,
-					"platform": "telegram",
-					"payload": {
-						"telegram": {
-							"text": "¡Elija su promoción!",
-							"reply_markup": {
-								"keyboard": descripcionPromo,
-								"one_time_keyboard": False,
-								"resize_keyboard": True
-								}
-							}
-						}
-				},
-				{
-					"type": 0,
-					"speech": "Lo siento, aún no configuro nada para whatsapbi"
-				}
-			]
-		}
 
 
 if __name__ == '__main__':
